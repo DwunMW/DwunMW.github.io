e0500f5446a857ec1763997814f02fc24d7297d5
DwunMW <34961640+DwunMW@users.noreply.github.com>
2018-01-11 22:05:51 +0100

    First Web Site Upload

---
+++ contact.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Manga Workout</title>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Oswald:300" rel="stylesheet">
+		<link rel="stylesheet" href="../Styles/Style.css">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100i" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600" rel="stylesheet">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:300" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	</head>
+	<body>
+	
+	
+		<header id="main">
+			<nav>
+				<img class="Menu" src="../image/Captue.JPG"/>
+				<label for="menu-mobile" class="menu-mobile">Menu</label>
+				<input type="Checkbox" id="menu-mobile" role="button">
+				<ul id="nav">
+					<li class="menu-index"><a href="index.html">Accueil</a></li>
+				
+					<li  class="menu-cuisine"><a href="nutrition.html">Nutrition</a>
+						<ul class="submenu">
+							<li ><a href="#">Recettes</a></li>
+							<li><a href="#">Aliments Pour ...</a></li>
+						</ul>
+					
+					</li>
+				
+					<li  class="menu-entrainements"><a href="entrainements.html">Entrainements</a>
+						<ul class="submenu">
+							<li><a href="#">S'entrainer Comme</a></li>
+							<li><a href="#">FromHumanToSayan</a></li>
+						</ul>
+					
+					</li>
+	
+					<li  class="menu-DLVV"><a href="DLVV.html">Dans la Vraie vie</a></li>
+			
+					<li  class="menu-autres"><a href="autres.html">Les autres vidéos</a>
+						<ul class="submenu">	
+							<li><a href="#">Final Flash</a></li>
+							<li><a href="#">Convention , vidéos pèches et FAQ</a></li>
+							<li><a href="#">Les mangas sport /shonen c'est la vie!</a></li>
+						</ul >
+		
+					</li>
+	
+					<li  class="menu-contact"><a href="../contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+		<div id="site-content">
+			
+			<div class="timeline">
+				<a class="twitter-timeline" data-lang="fr" data-width="250" data-height="500" data-theme="dark" href="https://twitter.com/MangaWorkout?ref_src=twsrc%5Etfw">Tweets by MangaWorkout</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><br/><br/><br/>
+				<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmangaworkout&tabs=timeline&width=250&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="250" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe><br/><br/><br/>
+			</div>
+			
+			<div class="contactglo">
+			
+				<div class="row">
+					<div class="col-md-6">
+						<form action="../post_contact" methode="POST">
+					<label style="color:white;" for="inputname">Nom :</label>
+					<input type="text" name="name" id="inputname">
+						</form>
+					</div>
+					
+					<div class="col-md-6">
+						<form action="../post_contact" methode="POST">
+					<label style="color:white;" for="inputemail">E-Mail :</label>
+					<input type="text" name="email" id="inputemail">
+						</form>
+						<label style="color:white;" for="inputmessage">Message</label>
+						<textarea id="inputmessage" name="message"></textarea>
+						<buttom type="submit"style="color:white;" >Envoyer</buttom>
+					</div>
+					
+					
+				</div>
+			</div>
+	
+		</div>
+			
+
+		<footer class="footer">
+						<div class="lienReseaux">
+			<a class="social" href="https://twitter.com/mangaworkout?lang=fr" target="_blank"><img src="../image/twitter.png" alt="social twitter"/><div class="fond-twitter"></div></a>
+			<a class="social" href="https://www.facebook.com/MangaWorkout/" target="_blank"><img src="../image/facebook.png" alt="social facebook"/><div class="fond-facebook"></div></a>
+			<a class="social" href="https://www.youtube.com/channel/UC4jJLUOBhHi3kT3zSuyn28g" target="_blank"><img src="../image/youtube1.png" alt="social youtube"/><div class="fond-youtube"></div></a>
+						</div><br/>
+				<p class="CopyRight"><em> by Manga Workout</em></p>
+			</footer>
+	
+	
+	</body>
+</html>
